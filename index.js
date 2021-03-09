4dfca16fb5a08abfb7db91a4af83c11353bad447
B-Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2021-03-09 14:31:36 +0100

    Session

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var express = require('express');
-var server = express();
+const express = require('express');
+const server = express();
+const cookieParser = require('cookie-parser');
+const { v4: uuidv4 } = require("uuid");
+
+server.use(express.json());
+server.use(cookieParser());
+
+server.use((req, res, next) => {
+    if (req.cookies.session !== undefined) {
+        next();
+    } else {
+        res.cookie("session", uuidv4(), { maxAge: 99999999999 });
+        next();
+    }
+});
 
 server.use(express.static(__dirname + '/public'));
 
